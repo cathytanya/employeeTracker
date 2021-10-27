b46e2b0a7a38712680e86846800965269df9e9e4
cathytanya <cathycolette.tanya@gmail.com>
2021-10-26 20:54:24 -0400

    made the connection between my js file and the data bases

--- employeeTacker.js
+++ employeeTacker.js
@@ -1,4 +1,16 @@
 // the dependencies needed for the application
 const sql = require("mysql2");
 const inquirer = require("inquirer");
+const consoleTable = require("console.table")
+// connecting to database
+const link = mysql.createConnection({
+    host:"localhost",
+    // port number 
+    port:3001,
+    // username/password on my personal server (SQL)
+    user:"root",
+    password:"Wildcats22",
+    // name of database used for the applicaton from SQL
+    database:"company_db"
+})
 
