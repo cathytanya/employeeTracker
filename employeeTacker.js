830c4102af3e23b6faffcebc51b30c0c729925b2
cathytanya <cathycolette.tanya@gmail.com>
2021-10-26 22:06:19 -0400

    listing the options the user will have to choose from a prompt in the termanial

--- employeeTacker.js
+++ employeeTacker.js
@@ -1,9 +1,11 @@
 // the dependencies needed for the application
 const sql = require("mysql2");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table")
+const consoleTable = require("console.table");
+const { Action } = require("rxjs/internal/scheduler/Action");
+const { prompt } = require("inquirer");
 // connecting to database
-const link = mysql.createConnection({
+const connection = mysql.createConnection({
     host:"localhost",
     // port number 
     port:3001,
@@ -13,4 +15,60 @@ const link = mysql.createConnection({
     // name of database used for the applicaton from SQL
     database:"company_db"
 })
+// sql server and sql database
+connection.connect(err =>{
+    if(err) throw err;
+    options();
+})
 
+// prompts the user will choose from 
+function options() {
+    inquirer.prompt({
+        name: "action",
+        type:"list",
+        message: "Welcome to our employee database. What would you like to do?",
+        // these are options which the user will choose from the database
+        choices:
+        ["View all Departments",
+        "View all Roles",
+        "View all Employees",
+        "Add Departments",
+        "Add Roles",
+        "Add Employees",
+        "Update Employees",
+        "Delete Employee",
+        "Exit"]
+    }).then (nextStep =>{
+        switch (nextStep.action){
+            case "View all Departments":
+                viewAllDepartments();
+            break;
+            case "View all Roles":
+                viewAllRoles();
+            break;
+            case "View all Employees":
+                viewAllEmployees();
+            break;
+            case "Add Departments":
+                addDepartments();
+            break;
+            case  "Add Roles":
+                addRoles();
+            break;
+            case "Add Employees":
+                addEmployees();
+            break;
+            case "Update Employees":
+                updateEmployees();
+            break;
+            case "Delete Employee":
+                deleteEmployees();
+            break;
+            case "Exit":
+                exit();
+            break;
+        }
+    })
+};
+// all the departments in database
+options();
